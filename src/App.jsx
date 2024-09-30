71cf863141fcafcca119f1078f9da95c2239df55
Dinesh Budhathoki <physmarika@gmail.com>
2024-09-30 11:33:23 +0800

    nav bar is completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Display } from "./components/Display";
+import { Hero } from "./components/Hero";
+
+function App() {
+  return (
+    <div className="wrapper">
+      {/* Hero section  */}
+      <Hero />
+      {/* Display section  */}
+      <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div>
+      <nav className="py-3 text-danger fixed-top">
+        <h2 className="container">MovieWorld</h2>
+      </nav>
+    </div>
+  );
+};
